f7c45870b8d30d4ab7f3f19963c7b625034789e5
Il <ilhan.a3@hotmail.com>
2018-09-23 07:57:08 +0200

    1) Leuchten der LEDs bedeutet jetzt Folgendes: - Rote LED: RH ist kleiner als 40 oder größer als 60 - Gelbe LED: Temperatur ist jetzt kleiner als 20°C oder größer als 23°C - Grüne LED: Beide Werte sind im guten Bereich (leuchtet nur, wenn Rot + Gelb aus sind)
    2) Code etwas verbessert und aufgeräumt.

--- ThermoAmpel/main.asm
+++ ThermoAmpel/main.asm
@@ -5,22 +5,19 @@
 .def RECEIVED_DATA_H = R23
 .def RECEIVED_DATA_L = R22
 .def MODE = R21
-.def MEAS_LB_H = R4 
-.def MEAS_LB_L = R5
-.def MEAS_UB_H = R6
-.def MEAS_UB_L = R7
-
+.def MEAS_LB_H = R6
+.def MEAS_LB_L = R7
+.def MEAS_UB_H = R8
+.def MEAS_UB_L = R9
 
 ;TODO www.mikrocontroller.net/articles/Entprellung
 
-.equ HUMIDITY_LB_H = 0b00000100 ; LB = Lowerbound, H = Highbits
-.equ HUMIDITY_LB_L = 0b00000101 ; L = Lowbits | Dec. Wert = 1029 - 40RH
+.equ HUMIDITY_MODE    = 0b00000101
+.equ TEMPERATURE_MODE = 0b00000011
+.equ SOFTRESET_MODE   = 0b00011110
 
-.equ HUMIDITY_UB_H = 0b00000111 ; 
-.equ HUMIDITY_UB_L = 0b00101101 ; 1837 -  60RH
+/* paar werte zum besseren einstellen 
 
-
-/*
 .equ HUMIDITY_LB_H = 0b00010011; LB = Lowerbound, H = Highbits
 .equ HUMIDITY_LB_L = 0b10010010 ; L = Lowbits | Dec. Wert = 5010 = 10°
 */
@@ -101,22 +98,69 @@ RESET:
 	ldi r16, (1<<TWPS0) | (1<<TWPS1)
 	out TWSR, r16
 
-	ldi r16, 0x00
-	mov RECEIVED_DATA_H, r16
-	mov RECEIVED_DATA_L, r16
-
+	;damit sensor funktioniert ist nach power-up oder softresetkommando 11 ms delay nötig
+	rcall DELAY_500MS
 
 MAIN:
-	ldi MODE, 0b00000101; 0b00000011 für Temp, 0b00000101 für Humidity
+	ldi MODE, HUMIDITY_MODE; 0b00000011 für Temp, 0b00000101 für Humidity
+	rcall LOAD_HUM_BOUNDS
 	rcall SEND_SENSOR
-	rcall CALC_HUMIDITY ; gerade auf temperatur eingestellt
+	rcall CALC_MEAS ; gerade auf temperatur eingestellt
 	cont_main:
 	rcall DELAY_500MS ; Maximum eine measurement pro sekunde bei 12 bit messungen
-	rcall DELAY_500MS ; daher lieber 2 sekunden warten vor neuer messung
+	rcall DELAY_500MS ; 
+	
+	ldi MODE, TEMPERATURE_MODE; 0b00000011 für Temp, 0b00000101 für Humidity
+	rcall LOAD_TEMP_BOUNDS
+	rcall SEND_SENSOR
+	rcall CALC_MEAS ; gerade auf temperatur eingestellt
+	cont_main2:
 	rcall DELAY_500MS
 	rcall DELAY_500MS
 
 rjmp MAIN
+
+; Grenzen für Relative Luftfeuchtigkeit in die dafür vorgesehenen Register laden
+LOAD_HUM_BOUNDS:
+	push r16
+
+	ldi r16, 0b00000100
+	mov MEAS_LB_H, r16
+	ldi r16, 0b00000101 ; L = Lowbits | Dec. Wert = 1029 - 40RH
+	mov MEAS_LB_L, r16
+
+	ldi r16, 0b00000111 ;
+	mov MEAS_UB_H, r16 
+	ldi r16, 0b00101101 ; 1837 -  60RH
+	mov MEAS_UB_L, r16
+
+/*	Bei dieser Einstellung sind Änderungen leichter zu sehen
+	
+	ldi r16, 0b00000100 ;
+	mov MEAS_UB_H, r16 
+	ldi r16, 0b11101101 ; 
+	mov MEAS_UB_L, r16
+*/
+	
+	pop r16
+ret
+
+; Grenzen für Temperatur in die dafür vorgesehenen Register laden
+LOAD_TEMP_BOUNDS:
+	push r16
+
+	ldi r16, 0b00010111 ; LB = Lowerbound, H = Highbits
+	mov MEAS_LB_H, r16
+	ldi r16, 0b01111010 ; L = Lowbits | Dec. Wert = 6010 = 20°
+	mov MEAS_LB_L, r16
+
+	ldi r16, 0b00011010 ; 
+	mov MEAS_UB_H, r16
+	ldi r16, 0b10100110 ; 6310 = 23°
+	mov MEAS_UB_L, r16
+
+	pop r16
+ret
 
 
 TOGGLE_ALL_LED:
@@ -168,12 +212,24 @@ ON_RED_LED:
 	sbi PORTA, PORTA5
 ret
 
-ON_RED_YELLOW:
+ON_YELLOW_LED:
 	sbi PORTA, PORTA6
 ret
 
-ON_RED_GREEN:
+ON_GREEN_LED:
 	sbi PORTA, PORTA7
+ret
+
+OFF_RED_LED:
+	cbi PORTA, PORTA5
+ret
+
+OFF_YELLOW_LED:
+	cbi PORTA, PORTA6
+ret
+
+OFF_GREEN_LED:
+	cbi PORTA, PORTA7
 ret
 
 INT0_BUTTON_MIDDLE:
@@ -250,7 +306,8 @@ DELAY_500MS:
 	pop r16
 
 ret
-		
+
+; Gehört zu DELAY_500MS / Kann für andere Delay-Routinen verwendet werden
 RESET_16BITCOUNTER:
 	push r16
 	push r17
@@ -276,126 +333,126 @@ RESET_16BITCOUNTER:
 ret
 
 
-CALC_TEMPERATURE:
-/* 
-17 = -40.1 + 0.01*x 
-x = 5710
-
-20 = -40.1 + 0.01*x 
-x = 6010
-
-23 = -40.1 + 0.01*x
-x = 6310
-*/
 
 
+;############################# BEGIN CALC_MEAS ##########################################
+;# Überprüft, ob vom Sender empfangene Bytes im korrekten Wertebereich sind und setzt	#
+;# entsprechend der Werte die LEDS														#
+;# Rote LED leuchtet:	Relative Luftfeuchtigkeit ist kleiner 40 oder größer 60			#
+;# Gelbe LED leuchtet:	Temperatur ist kleiner 20°C oder größer 23°C					#
+;# Grüne LED leuchtet:	Relative Luftfeuchtigkeit zwischen 40-60 und					#
+;#						Temperatur zwischen 20°C - 23°C									#
+;########################################################################################
 
-;---------Begin CALC_HUMIDITY-----------------
-CALC_HUMIDITY:
+CALC_MEAS:
 		push r16
-		/*Gutes RH im Wohnzimmer zwischen 40-60%
-		40 = -2.0468 + 0.0367 * x - 1.5955*10^(-6) * x²
-		x = 1029
 
-		60 = -2.0468 + 0.0367 * x - 1.5955*10^(-6) * x²
-		x = 1837
-
-		70 = -2.0468 + 0.0367 * x - 1.5955*10^(-6) * x²
-		x = 2167
-
-		75 = -2.0468 + 0.0367 * x - 1.5955*10^(-6) * x²
-		x = 2336
-
-		80 = -2.0468 + 0.0367 * x - 1.5955*10^(-6) * x²
-		x = 2509
-
-		TODO: vllt ab 60 gelb und ab 70 rot? (ab 70 kann sich schimmel bilden, ab 80 fürn langen zeitraum bildet sich sicher schimmel)
-
-		das heißt guter Wert x für RH ist: 
-		1029 < x < 1837
-
-		1029: 00000100 00000101
-		1837: 00000111 00101101
-		*/
-
-		/* TODO: bsp werte entfernen*/ 
-		;ldi RECEIVED_DATA_H, 0b00000100
-		;ldi RECEIVED_DATA_L, 0b00000101
-		;cpi RECEIVED_DATA_L, HUMIDITY_UB_L
-		;brlo BAD_LED_STATE ;hier
-		;rcall GOOD_LED_STATE
-
-		; cpi RECEIVED_DATA_H, 0x00
-		; breq test_state ;hier
-
-		cpi RECEIVED_DATA_H, HUMIDITY_LB_H ; Erst High bits der unteren Grenze checken
+		cp RECEIVED_DATA_H, MEAS_LB_H ; Erst High bits der unteren Grenze checken
 		brlo BAD_LED_STATE 				   ; Branch if Lower (Unsigned) | Wenn data < humidity -> LED ROT 
 		breq maybe_bad_state
 	
-	continue_calc_humidity:
-		ldi r16, HUMIDITY_UB_H
+	continue_CALC_MEAS:
+		mov r16, MEAS_UB_H
 		cp r16, RECEIVED_DATA_H ; wenn r16 < data -> LED ROT
 		brlo BAD_LED_STATE
 		breq maybe_bad_state2
-	continue_calc_humidity2:
+	continue_CALC_MEAS2:
 		rjmp GOOD_LED_STATE
 				
-	continue_calc_humidity3:
+	continue_CALC_MEAS3:
 		pop r16
+
+		cpi MODE, TEMPERATURE_MODE
+		breq calc_temp_mode
 		rjmp cont_main
 ret 
 
-
-test_state:
-	rcall TOGGLE_ALL_LED
-	rcall DELAY_500MS
-	rcall TOGGLE_ALL_LED
-	rcall DELAY_500MS
-	rjmp test_state
-ret
+calc_temp_mode:
+	jmp cont_main2
 
 maybe_bad_state:
-	cpi RECEIVED_DATA_L, HUMIDITY_LB_L ; Dann Low bits der unteren Grenze checken
+	cp RECEIVED_DATA_L, MEAS_LB_L ; Dann Low bits der unteren Grenze checken
 	brlo BAD_LED_STATE
-	rjmp continue_calc_humidity
+	rjmp continue_CALC_MEAS
 ret
 
 maybe_bad_state2:
-	push r16
-	ldi r16, HUMIDITY_UB_L
+	mov r16, MEAS_UB_L
 	cp r16, RECEIVED_DATA_L
 	brlo BAD_LED_STATE
-	rjmp continue_calc_humidity2
+	rjmp continue_CALC_MEAS2
 ret
-;---------End CALC_HUMIDITY-----------------
-
 
 BAD_LED_STATE:
-	; Toggle rote LED
-	rcall TOGGLE_RED_LED
-	rcall DELAY_500MS
-	rcall TOGGLE_RED_LED
-	rjmp continue_calc_humidity3
+	cpi MODE, TEMPERATURE_MODE
+	breq BAD_TEMPERATURE
+	brne BAD_HUMIDITY
+	cont_BAD_LED_STATE:
+	rjmp continue_CALC_MEAS3
 ret
+
+BAD_TEMPERATURE:
+	rcall OFF_GREEN_LED
+	rcall ON_YELLOW_LED
+	jmp cont_BAD_LED_STATE
+
+BAD_HUMIDITY:
+	rcall OFF_GREEN_LED
+	rcall ON_RED_LED
+	jmp cont_BAD_LED_STATE
 
 GOOD_LED_STATE:
-	; Toggle grüne LED
-	rcall TOGGLE_GREEN_LED
-	rcall DELAY_500MS
-	rcall TOGGLE_GREEN_LED
-	rjmp continue_calc_humidity3
+	push r16
+	push r17
+
+	cpi MODE, TEMPERATURE_MODE
+	breq GOOD_TEMPERATURE
+	brne GOOD_HUMIDITY
+	cont_GOOD_LED_STATE:
+
+	; Check if temperature and humidity is good by checking state of LED
+	ldi r16, (1<<PORTA6) | (1<<PORTA5)
+	in r17, PINA
+	and r17, r16
+	cpi r17, 0x00
+	; if both good -> GREEN LED ON
+	breq GOOD_TEMP_HUM
+	cont_GOOD_LED_STATE2:
+
+	pop r17
+	pop r16
+	rjmp continue_CALC_MEAS3
 ret
 
+GOOD_TEMPERATURE:
+	rcall OFF_YELLOW_LED
+	jmp cont_GOOD_LED_STATE
+
+GOOD_HUMIDITY:
+	rcall OFF_RED_LED
+	jmp cont_GOOD_LED_STATE
+
+GOOD_TEMP_HUM:
+	rcall ON_GREEN_LED
+	jmp cont_GOOD_LED_STATE2
+
+;############################### End CALC_MEAS #########################################
 
 
 
-; TWI FUNKTIONIERT NICHT, DAHER FOLGENDES:
-;DELAY_11MS
+;############################# BEGIN SEND_SENSOR ########################################
+;# Kommuniziert mit dem SHT75 Sensor. Sendet je nachdem wie MODE eingestellt ist		#
+;# eine Anfrage für die Temperatur (TEMPERATURE_MODE) oder Relative Luftfeuchtigkeit	#  
+;# (HUMIDITY_MODE). RECEIVED_DATA_H und RECEIVED_DATA_L beinhalten die Antwort des		#
+;# Sensors.																				#
+;########################################################################################
+
 ; PC1 SDA (DATA Pin am Sensor), PC0 SCL(SCK)
 SEND_SENSOR:
 	; als output setzen
 	sbi DDRC, PORTC1
 	sbi DDRC, PORTC0
+	; TODO nicht nach jedem mal reset_sequence starten, aber stört nicht wenn dabei ist
 	rcall RESET_SEQUENCE
 
 	; Transmission Start
@@ -491,21 +548,11 @@ SEND_SENSOR:
 	rcall READ_BYTE
 	mov CHECKSUM, RECEIVED_BYTE
 
-	/*	
-	rcall ENABLE_DATA
-	; DATA HIGH
-	sbi PORTC, PORTC1
-
-	rcall CLOCK_HIGH_LOW
-	*/
 ret
 
-/*
-checksum_error:
-	rcall TOGGLE_RED_LED
-ret
-*/
 ERROR_NO_ACK:
+	;TODO eventuell transmission neustarten lassen wenn kein ack gekommen ist. oder einfach reset knopf drücken
+
 	rcall TOGGLE_RED_LED
 	rcall DELAY_500MS
 	rcall TOGGLE_YELLOW_LED
@@ -615,166 +662,6 @@ RESET_SEQUENCE:
 	pop r16
 ret
 
-	
-SEND_HUMIDITY_REQ:
-	
-	; 1. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-	
-	; 2. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 3. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 4. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 5. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 6. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 7. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 8. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-ret
-
-SEND_TEMPERATURE_REQ:
-	
-	; 1. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-	
-	; 2. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 3. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 4. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 5. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 6. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 7. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 8. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-ret
-
-SEND_SOFTRESET_REQ:
-	
-	; 1. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-	
-	; 2. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 3. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 4. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 5. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 6. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 7. Übertragen: 1
-	sbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	; 8. Übertragen: 0
-	cbi PORTC, PORTC1
-
-	; Clock hoch und tiefsetzen für übertragung
-	rcall CLOCK_HIGH_LOW
-
-	;11ms delay bei softreset nötig
-	rcall DELAY_500MS
-
-ret
-
 SEND_COMMAND:
 	push r16
 	push r17
@@ -802,6 +689,11 @@ SEND_COMMAND:
 		dec r16
 	brne loop_command
 
+	cpi MODE, SOFTRESET_MODE
+	breq delay_for_softreset
+	cont_send_command:
+		
+
 	pop r18
 	pop r17
 	pop r16
@@ -827,196 +719,16 @@ send_zero:
 	
 	rjmp cont_loop_command
 ret
-/* Für "debug" zwecke
-RECEIVED_ACK:
-	rcall TOGGLE_GREEN_LED
+
+delay_for_softreset:
+	rcall TOGGLE_YELLOW_LED
 	rcall DELAY_500MS
-	rcall DELAY_500MS
-	rcall TOGGLE_GREEN_LED
-	rcall DELAY_500MS
-	rcall DELAY_500MS
-	rcall TOGGLE_GREEN_LED
-	rjmp main
-ret
+	rcall TOGGLE_YELLOW_LED
+	; TODO transstart nach softwarereset initialisieren + MODE überschreiben
+	; BSP:
+	; ldi MODE, HUMIDITY_MODE
+	; rcall SEND_SENSOR
+	; eventuell software reset bei einem interrupt?
+	rjmp cont_send_command
 
-DISMISSED_ACK:
-	rcall TOGGLE_RED_LED
-	rcall DELAY_500MS
-	rcall DELAY_500MS
-	rcall TOGGLE_RED_LED
-	rcall DELAY_500MS
-	rcall DELAY_500MS
-	rcall TOGGLE_RED_LED
-	rjmp main
-ret
-*/ 
-
-/*
-;***************************************************************************
-;*
-;* "mpy16u" - 16x16 Bit Unsigned Multiplication
-;*
-;* This subroutine multiplies the two 16-bit register variables 
-;* mp16uH:mp16uL and mc16uH:mc16uL.
-;* The result is placed in m16u3:m16u2:m16u1:m16u0.
-;*  
-;* Number of words	:14 + return
-;* Number of cycles	:153 + return
-;* Low registers used	:None
-;* High registers used  :7 (mp16uL,mp16uH,mc16uL/m16u0,mc16uH/m16u1,m16u2,
-;*                          m16u3,mcnt16u)	
-;*
-;***************************************************************************
-
-;***** Subroutine Register Variables
-
-.def	mc16uL	=r16		;multiplicand low byte
-.def	mc16uH	=r17		;multiplicand high byte
-.def	mp16uL	=r18		;multiplier low byte
-.def	mp16uH	=r19		;multiplier high byte
-.def	m16u0	=r18		;result byte 0 (LSB)
-.def	m16u1	=r19		;result byte 1
-.def	m16u2	=r20		;result byte 2
-.def	m16u3	=r21		;result byte 3 (MSB)
-.def	mcnt16u	=r22		;loop counter
-
-;***** Code
-
-mpy16u:	clr	m16u3		;clear 2 highest bytes of result
-	clr	m16u2
-	ldi	mcnt16u,16	;init loop counter
-	lsr	mp16uH
-	ror	mp16uL
-
-m16u_1:	brcc	noad8		;if bit 0 of multiplier set
-	add	m16u2,mc16uL	;add multiplicand Low to byte 2 of res
-	adc	m16u3,mc16uH	;add multiplicand high to byte 3 of res
-noad8:	ror	m16u3		;shift right result byte 3
-	ror	m16u2		;rotate right result byte 2
-	ror	m16u1		;rotate result byte 1 and multiplier High
-	ror	m16u0		;rotate result byte 0 and multiplier Low
-	dec	mcnt16u		;decrement loop counter
-	brne	m16u_1		;if not done, loop more
-	ret
-
-
-;***************************************************************************
-;*
-;* "div16u" - 16/16 Bit Unsigned Division
-;*
-;* This subroutine divides the two 16-bit numbers 
-;* "dd8uH:dd8uL" (dividend) and "dv16uH:dv16uL" (divisor). 
-;* The result is placed in "dres16uH:dres16uL" and the remainder in
-;* "drem16uH:drem16uL".
-;*  
-;* Number of words	:19
-;* Number of cycles	:235/251 (Min/Max)
-;* Low registers used	:2 (drem16uL,drem16uH)
-;* High registers used  :5 (dres16uL/dd16uL,dres16uH/dd16uH,dv16uL,dv16uH,
-;*			    dcnt16u)
-;*
-;***************************************************************************
-
-;***** Subroutine Register Variables
-
-.def	drem16uL=r14
-.def	drem16uH=r15
-.def	dres16uL=r16
-.def	dres16uH=r17
-.def	dd16uL	=r16
-.def	dd16uH	=r17
-.def	dv16uL	=r18
-.def	dv16uH	=r19
-.def	dcnt16u	=r20
-
-;***** Code
-
-div16u:	clr	drem16uL	;clear remainder Low byte
-	sub	drem16uH,drem16uH;clear remainder High byte and carry
-	ldi	dcnt16u,17	;init loop counter
-d16u_1:	rol	dd16uL		;shift left dividend
-	rol	dd16uH
-	dec	dcnt16u		;decrement counter
-	brne	d16u_2		;if done
-	ret			;    return
-d16u_2:	rol	drem16uL	;shift dividend into remainder
-	rol	drem16uH
-	sub	drem16uL,dv16uL	;remainder = remainder - divisor
-	sbc	drem16uH,dv16uH	;
-	brcc	d16u_3		;if result negative
-	add	drem16uL,dv16uL	;    restore remainder
-	adc	drem16uH,dv16uH
-	clc			;    clear carry to be shifted into result
-	rjmp	d16u_1		;else
-d16u_3:	sec			;    set carry to be shifted into result
-	rjmp	d16u_1
-	*/ 
-	
-	/*	Routine zum multiplizieren
-	; [ 16x16 Bit Unsigned Multiplication ]
-; multiplicand: r17:r16
-; multiplier  : r23:r22
-; result out  : r21:r20:r19:r18
-
-; 24 cycles
-	ldi r16, 0b00110001
-	ldi r17, 0b00000100
-
-	ldi r22, 0b00110001
-	ldi r23, 0b00000100
-	
-M_16x16:
-    MUL   r17, r23              ; ah * bh
-    MOVW  r21:r20, r1:r0
-    MUL   r16, r22              ; al * bl
-    MOVW  r19:r18, r1:r0
-    MUL   r17, r22              ; ah * bl
-    CLR   r22
-    ADD   r19, r0
-    ADC   r20, r1
-    ADC   r21, r22
-    MUL   r23, r16              ; bh * al
-    ADD   r19, r0
-    ADC   r20, r1
-    ADC   r21, r22
-RET
-
-*/
-
-/*Alte Codeblöcke*/
-/* Multiplikation beispiel
-	ldi mc16uL, 0b00110001
-	ldi mc16uH, 0b00000100
-
-	ldi mp16uL, 0b00110001
-	ldi mp16uH, 0b00000100
-
-	rcall mpy16u
-	*/
-
-	/*
-	RH = c1 + c2 * SO + c3 * SO²
-	c1: -2.0468
-	c2: 0.0367
-	c3: -1.5955*10^(-6)
-
-	RH = (367 * SO - 20468 - 159,55*SO/100*SO*100) / 10000
-
-	20468: 01001111 11110100
-
-	**********NICHT NÖTIG*******
-	*/
-
-
-
-	/*	ldi r17, -2.0468
-		ldi r18, 255
-		
-		;fmuls r18, r17
-		;movw r18:r17,r1:r0 ; Copy result back in r23:r22
-		
-		ldi r22, 2.0468
-		ldi r23, 127
-		muls r23,r22 ; Multiply signed r23 and r22 in (1.7) format, result in (1.15) format
-		movw r23:r22,r1:r0 ; Copy result back in r23:r22
-		*/
+;############################# END SEND_SENSOR #########################################
